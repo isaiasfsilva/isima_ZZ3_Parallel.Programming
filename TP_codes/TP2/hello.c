06a1041fd22eda77beacc64eed204b6c825338dd
Isaias Faria - Serveux ISIMA <isaias@isaiasfaria.com.br>
2018-01-22 13:24:28 +0100

    TP2 - First part OK

--- TP_codes/TP2/hello.c
+++ TP_codes/TP2/hello.c
@@ -17,20 +17,25 @@ DESCRIPTION:
 
 INSTRUCTIONS:
 		How to compile: mpicc –o hello hello.c
-		How to execute: ./hello
+		How to execute: mpirun -np 32 -hostfile myhostfile hello
 			
 			
 Last update: 22 january 2018
 
 This project is also available at Github
 	>> https://goo.gl/23qq51 
-	
+
 */
 
 
 
 #include "mpi.h"
 #include <stdio.h>
+//Aditional libs section
+   // Lib to use sched_getcpu()
+   #include <sched.h>
+
+
 int main(int argc, char **argv){
 
 //  // ================== QUESTIONS 1 AND 2 ======================= //
@@ -40,18 +45,84 @@ int main(int argc, char **argv){
 
 //  // ======================= QUESTION 3 ========================= //
 	// Executing with: mpirun -np 4 hello                           //
-	//  Hello from proc. 0 of 4 at processor node28.rcisima.isima.fr// 
-	//  Hello from proc. 2 of 4 at processor node28.rcisima.isima.fr//
- 	//  Hello from proc. 1 of 4 at processor node28.rcisima.isima.fr// 
- 	//  Hello from proc. 3 of 4 at processor node28.rcisima.isima.fr// 
- 	// -------------------------------------------------------------//
-	// Executing with: mpirun -np 4 hello                           //
-	//  Hello from proc. 0 of 4 at processor node28.rcisima.isima.fr// 
-	//  Hello from proc. 2 of 4 at processor node28.rcisima.isima.fr//
- 	//  Hello from proc. 1 of 4 at processor node28.rcisima.isima.fr// 
- 	//  Hello from proc. 3 of 4 at processor node28.rcisima.isima.fr// 
+    //  Hello from proc. 0 of 4 at processor node28.rcisima.isima.fr//
+    //    which id is 1                                             //
+    //  Hello from proc. 2 of 4 at processor node28.rcisima.isima.fr//
+    //    which id is 16                                            //
+    //  Hello from proc. 3 of 4 at processor node28.rcisima.isima.fr//
+    //    which id is 24                                            //
+    //  Hello from proc. 1 of 4 at processor node28.rcisima.isima.fr//
+    //    which id is 8                                             // 
  	// =============================================================//
 
+//  // ====================== QUESTION 4 =========================== //
+	// After had created the hostfile file, I executed with -np 32   //
+	// and defining the hostfile and the output was:                 //
+ 	// Hello from proc. 25 of 32 at processor node28.rcisima.isima.fr//
+	// which id is 11                                                //
+ 	// Hello from proc. 20 of 32 at processor node28.rcisima.isima.fr//
+	// which id is 2                                                 //
+ 	// Hello from proc. 16 of 32 at processor node28.rcisima.isima.fr//
+	// which id is 3                                                 //
+ 	// Hello from proc. 17 of 32 at processor node28.rcisima.isima.fr//
+	// which id is 15                                                //
+ 	// Hello from proc. 21 of 32 at processor node28.rcisima.isima.fr//
+	// which id is 14                                                //
+ 	// Hello from proc. 31 of 32 at processor node28.rcisima.isima.fr//
+	// which id is 10                                                //
+ 	// Hello from proc. 18 of 32 at processor node28.rcisima.isima.fr//
+	// which id is 1                                                 //
+ 	// Hello from proc. 19 of 32 at processor node28.rcisima.isima.fr//
+	// which id is 24                                                //
+ 	// Hello from proc. 22 of 32 at processor node28.rcisima.isima.fr//
+	// which id is 17                                                //
+ 	// Hello from proc. 23 of 32 at processor node28.rcisima.isima.fr//
+	// which id is 9                                                 //
+ 	// Hello from proc. 27 of 32 at processor node28.rcisima.isima.fr//
+	// which id is 12                                                //
+ 	// Hello from proc. 30 of 32 at processor node28.rcisima.isima.fr//
+	// which id is 6                                                 //
+ 	// Hello from proc. 24 of 32 at processor node28.rcisima.isima.fr//
+	// which id is 4                                                 //
+ 	// Hello from proc. 26 of 32 at processor node28.rcisima.isima.fr//
+	// which id is 16                                                //
+ 	// Hello from proc. 28 of 32 at processor node28.rcisima.isima.fr//
+	// which id is 0                                                 //
+ 	// Hello from proc. 29 of 32 at processor node28.rcisima.isima.fr//
+	// which id is 8                                                 //
+ 	// Hello from proc. 0 of 32 at processor node26.rcisima.isima.fr //
+	// which id is 7                                                 //
+ 	// Hello from proc. 2 of 32 at processor node26.rcisima.isima.fr //
+	// which id is 1                                                 //
+ 	// Hello from proc. 4 of 32 at processor node26.rcisima.isima.fr //
+	// which id is 20                                                //
+ 	// Hello from proc. 3 of 32 at processor node26.rcisima.isima.fr //
+	// which id is 28                                                //
+ 	// Hello from proc. 5 of 32 at processor node26.rcisima.isima.fr //
+	// which id is 10                                                //
+ 	// Hello from proc. 7 of 32 at processor node26.rcisima.isima.fr //
+	// which id is 14                                                //
+ 	// Hello from proc. 1 of 32 at processor node26.rcisima.isima.fr //
+	// which id is 24                                                //
+ 	// Hello from proc. 6 of 32 at processor node26.rcisima.isima.fr //
+	// which id is 22                                                //
+ 	// Hello from proc. 8 of 32 at processor node26.rcisima.isima.fr //
+	// which id is 5                                                 //
+ 	// Hello from proc. 9 of 32 at processor node26.rcisima.isima.fr //
+	// which id is 15                                                //
+ 	// Hello from proc. 11 of 32 at processor node26.rcisima.isima.fr//
+	// which id is 9                                                 //
+ 	// Hello from proc. 12 of 32 at processor node26.rcisima.isima.fr//
+	// which id is 2                                                 //
+ 	// Hello from proc. 13 of 32 at processor node26.rcisima.isima.fr//
+	// which id is 13                                                //
+ 	// Hello from proc. 14 of 32 at processor node26.rcisima.isima.fr//
+	// which id is 0                                                 //
+ 	// Hello from proc. 10 of 32 at processor node26.rcisima.isima.fr//
+	// which id is 6                                                 //
+ 	// Hello from proc. 15 of 32 at processor node26.rcisima.isima.fr//
+	// which id is 8                                                 //
+	// ==============================================================//
 
 //aux variables
 	char name[200]; // To save the processor's name
@@ -62,7 +133,7 @@ int main(int argc, char **argv){
 	MPI_Comm_rank( MPI_COMM_WORLD, &rang );
 	MPI_Comm_size( MPI_COMM_WORLD, &nbProcs );
 	MPI_Get_processor_name(name, &len);
-	printf( " Hello from proc. %d of %d at processor %s \n ",rang, nbProcs,name);
+	printf( " Hello from proc. %d of %d at processor %s which id is %d\n ",rang, nbProcs,name,sched_getcpu());
 	
 
 	MPI_Finalize();
---
+++ TP_codes/TP2/p2p_Question6.c
@@ -0,0 +1,69 @@
+/*
+
+[2018] Isaias Faria Silva 
+All Rights Reserved.
+ 
+NOTICE: All information contained here in this file is the 
+        property of Isaias Faria Silva. 
+        If you want to use this, please put the contact 
+        rights information.
+ 
+PURPOSE:Practical activity of Parallel Programming
+		Clermont-Auvergne University - Computer Science Institute
+		ZZ3 - 2018 January 
+
+DESCRIPTION:
+		This is just a communication test using MPI
+
+INSTRUCTIONS:
+		How to compile: mpicc –o p2p_Question6 p2p_Question6.c
+		How to execute: mpirun -np 32 -hostfile myhostfile p2p_Question6
+			
+			
+Last update: 22 january 2018
+
+This project is also available at Github
+	>> https://goo.gl/23qq51 
+
+*/
+
+#include "mpi.h"
+#include <stdio.h>
+#include <string.h>
+#include <string.h>
+int main(int argc, char **argv){
+
+
+	int rang, nbProcs, dest=0, source, etiquette = 50;
+	MPI_Status statut;
+	char message[300];
+	char messageS[300];
+	sprintf( message, " ");
+	MPI_Init( &argc, &argv );
+	MPI_Comm_rank( MPI_COMM_WORLD, &rang );
+	MPI_Comm_size( MPI_COMM_WORLD, &nbProcs );
+
+	//Everyone except the last thread has to read something from its succ
+	if(rang<(nbProcs-1)){
+		MPI_Recv( message, 300, MPI_CHAR, rang+1,etiquette, MPI_COMM_WORLD, &statut );
+	}	
+	//Everyone except the first thread has to send something to its pred
+	if ( rang != 0 ) {		
+		
+		//----------------------------------------------------//
+		// This part is so much important!!! Here we desloc 5 //
+		// caracters to right and put in the first 5 positions//
+		// my contribuition P MYID.                           //
+		//----------------------------------------------------//
+		sprintf( messageS, ", P%d%*.*s" , rang,5,295,message);
+
+		//send to my predecessor
+		MPI_Send( messageS,strlen(messageS)+1, MPI_CHAR,rang-1, etiquette, MPI_COMM_WORLD );
+	}else{
+		//Just the first thread has to print something
+		printf( "P0%s", message );
+	}
+
+	MPI_Finalize();
+	return 0 ;
+}
